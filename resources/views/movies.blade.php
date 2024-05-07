272928a81fa41231ce414faea9d7ae75002a6d23
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-07 19:00:42 +0200

    add movies page style

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,12 +2,13 @@
 
 @section ('content')
 
-<div class="container mt-5">
-    <div class="row row-cols-3 g-5">
+<div class="container-fluid mt-5 movies-container">
+    <div class="row movies-row row-cols-3 g-5 mx-auto p-5">
         @foreach ($movies as $movie)
         <div class="col">
             <div class="card">
                 <img class="card-img-top movie_card" src="{{$movie['thumb']}}" alt="">
+                <i class="fa fa-play text-light" aria-hidden="true"></i>
                 <div class="card-body fs-3 text-center fw-lighter">
                     {{$movie['title']}}
                 </div>
