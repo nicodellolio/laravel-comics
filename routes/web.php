d56821823ea01e64a3cbbfe613acc1152338030f
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-05-07 16:33:07 +0200

    add partials + add header

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    
+    $data = [
+        'nav_items' => [
+            'characters',
+            'comics',
+            'movies',
+            'tv',
+            'games',
+            'collectibles',
+            'videos',
+            'fans',
+            'news',
+            'shop',
+        ]
+    ];
+    return view('home', $data);
 });
